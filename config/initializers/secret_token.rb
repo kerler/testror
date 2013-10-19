386db5f49888e60d931b997412fceb3b78d42fe3
liubaogang <liubaogang@hotmail.com>
2013-10-19 10:30:26 +0800

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lbgnews::Application.config.secret_token = '8601db89269ccc42a0e5aa50653826170c9d510fffa27cb0367facadf9f77f1c1046a2b0e1b6cb6bd89e29cde12722f790eca8273c92b5255c45ad9f8af0123a'
